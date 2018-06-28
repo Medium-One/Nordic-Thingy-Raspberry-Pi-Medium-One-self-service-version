a90816a5ae0eb5e88d3b81d782c1a1bf0f4c1634
Michael Li <mikelisfbay@yahoo.com>
2018-06-27 17:29:03 -0700

    Update steps_taken_alert.py

--- M1_workflow_python_code/steps_taken_alert.py
+++ M1_workflow_python_code/steps_taken_alert.py
@@ -2,18 +2,23 @@ import Store
 import FreeSMS
 import Email
  
-email_address = 'demo@medium.one'
-phone_number = '1234567890'
+email_address = "you@demo.com"
+phone_number = "1234567890"
  
  
 last_steps = Store.get('last_steps')
 if not last_steps:
     last_steps = 0 
+else:
+    last_steps = int(last_steps)
  
 steps = IONode.get_input('in1')['event_data']['value']
 current_step_count = steps - last_steps
-Store.set_data('last_steps', steps)
+
+Store.set_data('last_steps', str(steps))
+
 if current_step_count > 10 and not Store.get('sent_alert'):
+    log('more than 10')  # debug
     alert_message = "Your Nordic Thingy has moved!"
     email = Email.Email(sender='alerts@medium.one', display_name='Medium One Alerts',
                         recipients=[email_address], subject='Alert: Motion Detected', message=alert_message, attachments=None)
